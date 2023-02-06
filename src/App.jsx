882acc49e3a143663f8026b32ab83623c0d858f5
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-06 01:03:04 +0000

    setting up single load

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import "./App.css";
 import Pinball from "./pages/Pinball";
 
 function App() {
-  const [gameHasMounted, setGameHasMounted] = useState(false);
-
-  // useEffect(() => {
-  //   setGameHasMounted(true);
-  // }, []);
-
-  return <div className="App">{gameHasMounted ? null : <Pinball />}</div>;
+  return (
+    <div className="App">
+      <Pinball />
+    </div>
+  );
 }
 
 export default App;
