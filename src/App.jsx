e1a0e0f5d56b22606c17b41058d26a089f7063cf
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-06 00:16:39 +0000

    added game

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+import Pinball from "./pages/Pinball";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameHasMounted, setGameHasMounted] = useState(false);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  // useEffect(() => {
+  //   setGameHasMounted(true);
+  // }, []);
+
+  return <div className="App">{gameHasMounted ? null : <Pinball />}</div>;
 }
 
-export default App
+export default App;
